281fd9697dd08be49fd60b3065d1819f08e7c235
Jennifer Kash <jenniferkash3@gmail.com>
2023-07-27 16:28:50 -0400

    update

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,16 @@ const app = express()
 const path = require('path')
 
 app.use(express.static(`public`))
+//  // include and initialize the rollbar library with your access token
+// var Rollbar = require('rollbar')
+// var rollbar = new Rollbar({
+//   accessToken: '0144beb79bd5405e875c2a95283bd24e',
+//   captureUncaught: true,
+//   captureUnhandledRejections: true,
+// })
+
+// // record a generic message and send it to Rollbar
+// rollbar.log('Hello world!')
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'../public/index.html'))
